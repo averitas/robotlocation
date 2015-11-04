bb3a101b958f1bfc1d3375b633aa138dc702e694
averitas <lewis9224@gmail.com>
2015-11-03 22:59:03 -0500

    new
    new

---
+++ robot.cc
@@ -0,0 +1,18 @@
+#include "robot.hpp"
+
+Robot::Robot(){
+	
+}
+bool Robot::move(int num,double r,double angle){
+	
+}
+void Robot::addRobot(int x,int y,int v){
+	
+}
+bool Robot::delRobot(int num){
+	
+}
+
+int Robot::getXY(int x,int y){
+	
+}
---
+++ robot.hpp
@@ -0,0 +1,34 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include <iostream>
+using namespace std;
+
+class Robot{
+public:
+	Robot();
+	bool move(int num,double r,double angle); //move the index of num robot, return false if not exist
+	void addRobot(int x=0,int y=0,int v=0);
+	bool delRobot(int num); //delete the robot that index of num, return false if not exist
+	
+private:
+    class Node{
+    public:
+    	int x;
+	    int y;	
+	    int velocity;
+	    Node(int x=0,int y=0,int v=0):x(x),y(y),velocity(v){
+		}
+		Node(Node p){
+			x=p.x;
+			y=p.y;
+			velocity=p.velocity;
+		}
+	};
+	Node *robots;
+	int number;
+	int getXY(int x,int y);
+	int *bitmap;   //size is 400*400
+};
+
+#endif // ROBOT_H
